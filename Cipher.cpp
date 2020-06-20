6f2c1bf97860dfd4b2c822882257fc4b0beaa60f
Aaron Kampmeier <aaron.kampmeier@gmail.com>
2020-06-20 13:54:14 -0500

    Added lots more large test files of classic literature. Began implementing a profiling tool.

--- Cipher.cpp
+++ Cipher.cpp
@@ -7,65 +7,161 @@
 
 #define FILE_ERROR_EXIT 1
 #define FILE_OUT_ERROR_EXIT 2
+#define UNKNOWN_OPERATION_EXIT 3
 
 // Defines how long a block should be when using BLOCKED CipherStyle
 #define BLOCK_CIPHER_LENGTH 5
 // Defines how many blocks are on a line when using BLOCKED
 #define BLOCK_CIPHER_LINE_GROUPS 6
 
+// For Profiling
+#define PROFILING 1
+
 #include <iostream>
-#include <cstdio>
 #include <cstring>
 
 using namespace std;
 
 /**
- * The different styles for enciphering.
- * Punctuated: Follows all original punctuation marks, only letters are enciphered.
- * Blocked: Removes all punctuation including spaces, letters are blocked into groups making it harder to break.
+ * Switches this program's function between enciphering and deciphering.
+ * There are a few different styles for enciphering:
+ * - Punctuated: Follows all original punctuation marks, only letters are enciphered.
+ * - Blocked: Removes all punctuation including whitespace, letters are blocked into groups making it decently harder
+ * 		to break.
  */
-enum CipherStyle {PUNCTUATED, BLOCKED};
+enum CipherOperation {ENCIPHER_PUNCTUATED, ENCIPHER_BLOCKED, DECIPHER, UNDEFINED};
 
-/**
- * Switches this program's function.
- */
-enum CipherOperation {ENCIPHER, DECIPHER};
 
-bool cipher(CipherOperation operation, CipherStyle cipherStyle, char *key, char *inputFile, char *outputFile);
+bool cipher(CipherOperation operation, char *key, char *inputFile, char *outputFile);
+CipherOperation convertStrToOp(const char *strOp);
+
+#if PROFILING
+void profileCipher();
+#endif
 
 /**
  * Program that en/deciphers text using a Vigenère cipher algorithm. A vigenère cipher is a poly-alphabetic cipher in
  * that we loop through the key for each character we en/decipher making it significantly more complicated to break
  * than a traditional Caesar shift.
  *
- * Enciphering can be done in a few different styles specified by CipherStyle. Deciphering will always decipher
- * letters and leave all non-letters in place.
+ * Takes in command line arguments, will prompt for console input if non-existent:
+ * - Structure: cipher [operation [key [inFile [outFile]]]]
+ * - Options:
+ * 	- Operation: encipher-blocked, encipher-punctuated, decipher
+ * 	- Key: Cipher key
+ * 	- inFile: Input file path
+ * 	- outFile: Output file path
+ *
  *
  * @return Exit code
  */
 int main(int argc, char** argv) {
-	// User inputted data
-	CipherOperation operation = ENCIPHER;
-	CipherStyle cipherStyle = BLOCKED;
-	char inFileName[] = "../War and Peace.txt";
-	char outFileName[] = "../War and Peace Enciphered.txt";
-	char key[] = "python";
-	
-	cipher(operation, cipherStyle, key, inFileName, outFileName);
-	
+#if PROFILING
+	profileCipher();
 	return 0;
+#endif
+	
+	CipherOperation operation = UNDEFINED;
+	char* inFileName = nullptr;
+	char* outFileName = nullptr;
+	char* key = nullptr;
+	
+	
+	// Check each command line argument, one by one
+	for(int argi = 1; argi < argc; argi++) {
+		// Check this argument against our known arguments
+		char* arg = argv[argi];
+		switch (argi) {
+			case 1:
+				// Operation
+				operation = convertStrToOp(arg);
+				break;
+			case 2:
+				//Key
+				key = arg;
+				break;
+			case 3:
+				// In file
+				inFileName = arg;
+				break;
+			case 4:
+				// Out file
+				outFileName = arg;
+				break;
+			default:
+				cerr << "Unknown input at index " << argi << endl;
+				break;
+		}
+	}
+	
+	// For each non-initialized variable, ask for input. If input needs to go longer than the specified sizes, you
+	// must use command line args for the input
+	if (operation == UNDEFINED) {
+		cout << "Enter the cipher operation (encipher-blocked, encipher-punctuated, decipher): " << endl;
+		char inputOprString[30];
+		cin.getline(inputOprString, sizeof(inputOprString));
+		operation = convertStrToOp(inputOprString);
+	}
+	
+	if (key == nullptr) {
+		cout << "Enter the cipher key: " << endl;
+		key = new char[30];
+		cin.getline(key, 30);
+	}
+	
+	if (inFileName == nullptr) {
+		cout << "Enter the input file path (based on working dir): " << endl;
+		inFileName = new char[100];
+		cin.getline(inFileName, 100);
+	}
+	
+	if (outFileName == nullptr) {
+		cout << "Enter the output file path (based on working dir): " << endl;
+		outFileName = new char[100];
+		cin.getline(outFileName, 100);
+	}
+	
+ 
+	// Perform the operation
+	bool success = cipher(operation, key, inFileName, outFileName);
+	
+	delete[] key;
+	delete[] inFileName;
+	delete[] outFileName;
+	
+	return !success;
 }
 
 /**
- * Performs the en/decipher operation
- * @param operation
- * @param cipherStyle
- * @param key
- * @param inputFile
- * @param outputFile
- * @return
+ * Converts a string to a CipherOperation
+ * @param strOp Either "encipher-blocked", "encipher-punctuated", or "decipher"
+ * @return The associated CipherOperation or UNDEFINED if failed
  */
-bool cipher(CipherOperation operation, CipherStyle cipherStyle, char *key, char *inputFile, char *outputFile) {
+CipherOperation convertStrToOp(const char *strOp) {
+	if (strcmp("encipher-blocked", strOp) == 0) {
+		return ENCIPHER_BLOCKED;
+	} else if (strcmp("encipher-punctuated", strOp) == 0) {
+		return ENCIPHER_PUNCTUATED;
+	} else if (strcmp("decipher", strOp) == 0) {
+		return DECIPHER;
+	} else {
+		// Unknown input
+		cerr << "Unknown operation" << endl;
+		exit(UNKNOWN_OPERATION_EXIT);
+		return UNDEFINED;
+	}
+}
+
+/**
+ * Performs the en/decipher operation writing the output to the specified output file. Enciphering can be done in a
+ * few different styles. Deciphering will always decipher letters and leave all non-letters in place.
+ * @param operation The operation to perform
+ * @param key The key
+ * @param inputFile The file to read from
+ * @param outputFile The file to write to, will overwrite current file if one exists.
+ * @return Successful
+ */
+bool cipher(CipherOperation operation, char *key, char *inputFile, char *outputFile) {
 	// Open the files to work with
 	FILE* inFile = fopen(inputFile, "r");
 	FILE* outFile = fopen(outputFile, "w");
@@ -82,12 +178,13 @@ bool cipher(CipherOperation operation, CipherStyle cipherStyle, char *key, char 
 	// The write buffer will be the same as the read buffer if doing normal PUNCTUATED. BLOCKED needs to use a
 	// different one with extra padding.
 	char* writeBuffer;
-	switch (cipherStyle) {
-		case PUNCTUATED:
+	switch (operation) {
+		case DECIPHER:
+		case ENCIPHER_PUNCTUATED:
 			// Write in place to the same buffer
 			writeBuffer = readBuffer;
 			break;
-		case BLOCKED:
+		case ENCIPHER_BLOCKED:
 			// Use a different buffer to write to in order to add block spacing
 			int maxSpaces = sizeof(readBuffer) / BLOCK_CIPHER_LENGTH;
 			writeBuffer = new char[sizeof(readBuffer) + maxSpaces];
@@ -147,38 +244,44 @@ bool cipher(CipherOperation operation, CipherStyle cipherStyle, char *key, char 
 			// re-convert back to ASCII by adding 65.
 			//4. Write cipher txt char back to write buffer
 			if (isalpha(readBuffer[bufferReadIndex])) {
-				writeBuffer[bufferWriteIndex] = (toupper(readBuffer[bufferReadIndex]) + keyOffset - 65) % 26 + 65;
+				switch (operation) {
+					case ENCIPHER_BLOCKED:
+					case ENCIPHER_PUNCTUATED:
+						writeBuffer[bufferWriteIndex] = (toupper(readBuffer[bufferReadIndex]) + keyOffset - 65) % 26 + 65;
+						break;
+					case DECIPHER:
+						// To decipher, just subtract the key offset from the cipher letter. However since this could
+						// yield negative numbers and the mod operator won't loop negative numbers in c++, we'll go
+						// around the other way by just adding the complement: (26 - keyOffset)
+						// i.e. -keyOffset = (26 - keyOffset) in mod 26
+						writeBuffer[bufferWriteIndex] = (toupper(readBuffer[bufferReadIndex]) + (26 - keyOffset) -
+								65) % 26 + 65;
+						break;
+				}
+				
+				bufferWriteIndex++;
 				
 				// We used a key letter so move on to the next
 				keyLetterIndex = (keyLetterIndex + 1) % keySize;
 				
-				bufferWriteIndex++;
-//				lineIndex++;
 				
-				if (cipherStyle == BLOCKED) {
+				if (operation == ENCIPHER_BLOCKED) {
 					blockIndex++;
 					
-//					if (lineIndex > 35) {
-//						cout << "hmm" << endl;
-//					}
-					
 					if (blockIndex / BLOCK_CIPHER_LENGTH == BLOCK_CIPHER_LINE_GROUPS) {
-//						cout << lineIndex << endl;
 						
 						// End the line, we have the right number of groups
 						writeBuffer[bufferWriteIndex] = '\n';
-//						lineIndex = 0;
 						bufferWriteIndex++;
 						blockIndex = 0;
 					} else if (blockIndex % BLOCK_CIPHER_LENGTH == 0) {
 						// Add in a space after every block
 						writeBuffer[bufferWriteIndex] = ' ';
 						bufferWriteIndex++;
-//						lineIndex++;
 					}
 				}
-			} else if (cipherStyle == PUNCTUATED) {
-				// A punctuation char and PUNCTUATED cipherStyle, so keep it in there
+			} else if (operation != ENCIPHER_BLOCKED) {
+				// This runs if our character is not alphabetic, so with all styles except BLOCKED, keep it in there
 				bufferWriteIndex++;
 			}
 			
@@ -190,15 +293,16 @@ bool cipher(CipherOperation operation, CipherStyle cipherStyle, char *key, char 
 		
 		// Check that everything wrote successfully
 		if (validWriteChunks != bufferWriteIndex * sizeof(char)) {
-			cerr << "Error writing to enciphered file." << endl;
+			cerr << "Error writing to ouput file." << endl;
 			return false;
+//			exit(FILE_OUT_ERROR_EXIT);
 		}
 		
 		processedBuffers++;
 	} while (validReadChunks == sizeof(readBuffer)); //Once valid read chunks is less than the whole buffer, we've
 	// reached the end of the file
 	
-	if (cipherStyle == BLOCKED) {
+	if (operation == ENCIPHER_BLOCKED) {
 		delete[] writeBuffer;
 	}
 	
@@ -209,4 +313,9 @@ bool cipher(CipherOperation operation, CipherStyle cipherStyle, char *key, char 
 	return true;
 }
 
+#if PROFILING
+void profileCipher() {
+
+}
+#endif
 
